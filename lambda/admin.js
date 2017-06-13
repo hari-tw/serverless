b6b22d942c6042505d702abd86f4c15a103c30a8
Felipe Hlibco <hlibco@gmail.com>
2017-06-13 02:11:02 -0700

    feat(serverless.yml): add authorizer using cognito identity pool (WIP)

--- lambda/admin.js
+++ lambda/admin.js
@@ -8,7 +8,7 @@ module.exports.handler = (event, context, callback) => {
   console.log('Admin.')
   const response = callback(null, Reply.success({
     admin: true,
-    cognitoPoolId: JSON.stringify(process.env.COGNITO_POOL_ID)
+    cognitoPoolId: process.env.COGNITO_POOL_ID
   }))
 
   callback(null, response)
